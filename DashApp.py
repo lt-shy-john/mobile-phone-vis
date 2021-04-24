9cd043d3e05b20afab009562367e34f587585a9d
lt-shy-john <john@shyjohn.net>
2021-04-24 21:52:49 +1000

    Update DashApp.py

--- DashApp.py
+++ DashApp.py
@@ -32,62 +32,62 @@ app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
     [Input('attOptions', 'value'),
     Input('yearSlider', 'value')])
 def update_figure(i, yearRange):
-    
+
     #Filter out data into specified year range
     isInYearRange = ((releaseYear >= yearRange[0]) * (releaseYear <= yearRange[1])).astype(bool)
     years = releaseYear[isInYearRange]
     att = attributeArray[i]
     att = att[isInYearRange]
     names = modelNames[isInYearRange].astype(str)
-    
+
     #Calculate linear fit based on filtered data
     linearFitX, linearFitY, detrendedAttributes = getLinearFit(years, att, useLogScale[i])
-    
+
     #We define the main scatter plot to be shown in our dashboard
     fig = go.Figure()
     fig.add_trace(go.Scatter(
-        x=years, 
-        y=att, mode='markers', 
-        hovertemplate=names, 
+        x=years,
+        y=att, mode='markers',
+        hovertemplate=names,
         name=attributeNames[i],
         marker=dict(size=8, color=detrendedAttributes, colorscale='Turbo')))
-    
+
     #Define the fitted line to be included in our scatter plot
     fig.add_trace(go.Scatter(x=linearFitX, y=linearFitY, name='Fit'))
-    
+
     #Use log scale if necessary
     if useLogScale[i]:
         fig.update_yaxes(type="log")
-    
+
     #We identify the market leader based on the max value in detrended attribute, and then create table data
     namee = np.copy(names)
     marketLeaderIds = (-detrendedAttributes).argsort()
     marketLeaderName = namee[marketLeaderIds]
     marketLeaderData = [{'phoneName': name} for name in marketLeaderName[:10]]
-        
+
     #Run transition animation and return values to dashboard objects
     fig.update_layout(transition_duration=500, showlegend=False)
     return fig, marketLeaderData
 
 #Returns coordinates of a line that fits the data linearly and detrended attributes
 def getLinearFit(years, att, useLog):
-    
+
     #Use log scale if necessary
     att = att.astype(float)
     if useLog:
         att[att==0] = 0.0001
         att = np.log(att)
-    
+
     #We get fit coefficients and create y values for a fitted line
     fitCoefficients = np.polyfit(years, att, 1)
     fitLeft = years[0] * fitCoefficients[0] + fitCoefficients[1]
     fitRight = years[-1] * fitCoefficients[0] + fitCoefficients[1]
-    
+
     #If using an exponential fit, we convert our fit back to the exponential scale
     if useLog:
         fitLeft = np.exp(fitLeft)
         fitRight = np.exp(fitRight)
-    
+
     #Create the final lists to return
     linearFitX = [years[0], years[-1]]
     linearFitY = [fitLeft, fitRight]
@@ -111,8 +111,8 @@ controls = dbc.Card([
         html.Label('Year:'),
         dcc.RangeSlider(
             id='yearSlider',
-            min=releaseYear[0], 
-            max=releaseYear[-1], step=0.1, 
+            min=releaseYear[0],
+            max=releaseYear[-1], step=0.1,
             value=[releaseYear[0], releaseYear[-1]],
             marks={1991: '1991', 2012: '2012'}),
         dash_table.DataTable(id='table', columns=[{'name': 'Phone Name', 'id': 'phoneName'}])
@@ -126,6 +126,19 @@ scatterContent = dbc.Container([
     dbc.Row([
         dbc.Col(controls, md=4),
         dbc.Col(dcc.Graph(id='scatterPlot'), md=8)
+    ])
+])
+
+#========================================== Code for Parallel Coordinates Page ======================================
+@app.callback(
+    [Output('paracoorPlot', 'figure'),
+    Output('table','data')])
+
+paracoorContent = dbc.Container([
+    html.H1('Models Showing the Trend'),
+    html.Hr(),
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='paracoorPlot'), md=8)
     ])
 ])
 
@@ -155,7 +168,8 @@ sidebar = html.Div([
     html.Hr(),
     dbc.Nav([
         dbc.NavLink('Home', href='/', active='exact'),
-        dbc.NavLink('Scatter Plots', href='/scatter', active='exact')    
+        dbc.NavLink('Scatter Plots', href='/scatter', active='exact')
+        dbc.NavLink('Parallel Coordinate Plot', href='/paracoor', active='exact')
     ])
 ], style=sidebarStyle)
 
@@ -165,14 +179,16 @@ app.layout = html.Div([dcc.Location(id='url'), sidebar, content])
 
 #Define the functionality of the sidebar
 @app.callback(
-Output('pageContent', 'children'), 
+Output('pageContent', 'children'),
 [Input('url', 'pathname')])
 def renderPageContent(pathname):
     if pathname == '/':
         return dbc.Container([html.P('This is the home page')])
     elif pathname == '/scatter':
         return scatterContent
-    
+    elif pathname == '/paracoor':
+        return paracoorContent
+
     #Return error if pathname does not exist
     return dbc.Jumbotron([
         html.H1("404: Not found", className="text-danger"),
